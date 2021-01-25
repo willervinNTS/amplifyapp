8dc3d1692b83166aa3591d741da836dbd0ee8c99
Will Ervin <william.ervin@nextechsol.com>
2021-01-25 11:14:34 -0500

    updates to App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
+import mantle_logo from './favicon2.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mantle_logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my simple React application
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://nextechsol.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit our website
         </a>
       </header>
     </div>
